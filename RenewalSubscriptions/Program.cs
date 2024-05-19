44f593290e3eb1eda028f4b63840e1dfda1444fb
Thatguycalledcoder <quarshiedavid62@gmail.com>
2024-05-19 23:38:00 +0000

    Completed challenge.

--- RenewalSubscriptions/Program.cs
+++ RenewalSubscriptions/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 1) {
+    Console.WriteLine("Your subscription expires within a day!\nRenew now and save 20%!");
+}
+else if (daysUntilExpiration <= 5) {
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save 10%!")
+}
+else if (daysUntilExpiration <= 10) 
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else {
+    Console.WriteLine("Your subscription has expired.");
+}
