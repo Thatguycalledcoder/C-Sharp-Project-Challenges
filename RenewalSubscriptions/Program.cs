bfbd7ef3d904e12557c2dfe432a9bd7ba48234e6
Thatguycalledcoder <quarshiedavid62@gmail.com>
2024-05-19 23:38:36 +0000

    Fixed syntax errors.

--- RenewalSubscriptions/Program.cs
+++ RenewalSubscriptions/Program.cs
@@ -6,7 +6,7 @@ if (daysUntilExpiration == 1) {
     Console.WriteLine("Your subscription expires within a day!\nRenew now and save 20%!");
 }
 else if (daysUntilExpiration <= 5) {
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save 10%!")
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save 10%!");
 }
 else if (daysUntilExpiration <= 10) 
 {
