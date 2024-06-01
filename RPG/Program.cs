d9be62835ce21283a8a07ca1d071cc88f5b931b0
Thatguycalledcoder <quarshiedavid62@gmail.com>
2024-06-01 02:49:06 +0000

    Added logic for RPG game.

---
+++ RPG/Program.cs
@@ -0,0 +1,28 @@
+﻿int playerHealth = 10;
+int monsterHealth = 10;
+bool playerTurn = true;
+
+Random attack = new();
+int damage = 0;
+
+do {
+    damage = attack.Next(1, 11);
+
+    if (playerTurn) {
+        monsterHealth -= damage;
+        Console.WriteLine($"Monster was damaged and lost {damage} health and now has {monsterHealth} health.");
+    }
+    else {
+        playerHealth -= damage;
+        Console.WriteLine($"Hero was damaged and lost {damage} health and now has {playerHealth} health.");
+    }
+
+    playerTurn = !playerTurn;
+} while ((playerHealth > 0) && (monsterHealth > 0));
+
+if (playerHealth > 0)
+    Console.WriteLine("Hero wins!");
+else 
+    Console.WriteLine("Hero loses!");
+
+Console.WriteLine("Game over!");
